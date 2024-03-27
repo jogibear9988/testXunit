dfb214dd186452d1fbbc94cd7caf9598d58d2bc1
jkuehner <jochen.kuehner@kardex.com>
2024-03-27 11:11:08 +0100

    test

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using MimeKit;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void aa()
+        {
+            using (var ms = new MemoryStream())
+            {
+                var parser = new MimeParser(ms, MimeFormat.Mbox);
+                while (!parser.IsEndOfStream && ms.Length>0)
+                {
+                    var message = parser.ParseMessage();
+                }
+            }
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using ClassLibrary1;
+using Xunit;
+namespace Microsoft.VisualStudio.TestTools.UnitTesting { }
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var cls1 = new Class1();
+            cls1.aa();
+        }
+    }
+}
